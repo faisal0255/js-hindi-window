d9d4cb1b32724bbb1c5ab7a0f47f41b7aa47c578
Faisal Raza <faisalraza841311@gmail.com>
2025-03-13 12:04:39 +0000

    increement and decrement and logical operations

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,25 @@
-console.log(typeof null);
+// console.log(typeof null);
+
+// let value = 3;
+// let negValue = -value;
+// console.log(value); 
+
+// *******Operations*********
+// console.log(4+4);
+// console.log(3-2);
+// console.log(4*5);
+// console.log(3/4);
+
+
+let gameCounter = 50;
+gameCounter++
+
+console.log(gameCounter);
+
+let x = 2;
+const y = x ++;
+ console.log(x);
+ console.log(y);
+ 
+
+
