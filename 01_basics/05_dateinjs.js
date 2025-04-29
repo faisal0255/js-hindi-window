83625b6e225f7ffd57d98bd64441e89bc2f6ed4f
Faisal Raza <faisalraza841311@gmail.com>
2025-04-29 08:15:11 +0000

    date in js

---
+++ 01_basics/05_dateinjs.js
@@ -0,0 +1,22 @@
+// Dates in JavaScript 
+
+let myDate = new Date();
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(typeof myDate);
+
+// let myOwnDate = new Date(2026 , 0 , 31);
+// console.log(myOwnDate.toDateString());
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getHours());
+console.log(newDate.getDay());
+console.log(newDate.getDate());
