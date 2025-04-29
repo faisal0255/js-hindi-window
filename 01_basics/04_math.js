427a6a5785c39a0b56e59d88376787299ceccf03
Faisal Raza <faisalraza841311@gmail.com>
2025-04-29 07:24:42 +0000

    adding some info related to .math function

---
+++ 01_basics/04_math.js
@@ -0,0 +1,14 @@
+// +++++++ Math ++++++++++
+
+// console.log(Math.random);
+// console.log(Math.ceil(3.00001));
+// console.log(Math.sqrt(25));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 15;
+const max = 25;
+
+console.log( Math.floor(Math.random() * (max - min +1 )) + min);
+ 
