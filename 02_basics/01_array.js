56ac7df96aaa4d1daf8ce1d743e96738677de149
Faisal Raza <faisalraza841311@gmail.com>
2025-05-01 15:05:50 +0000

    array in js

---
+++ 02_basics/01_array.js
@@ -0,0 +1,13 @@
+const myarr = [3,3,4,5,2,6];
+
+myarr.push(10);  // adding element to the last of the array
+myarr.push(23);  
+myarr.pop();  // removing array from last of the array
+myarr.unshift(90); // adding element  in starting of the array
+myarr.shift();  // removing  element  in starting of the array
+myarr.shift();
+myarr.shift()
+
+console.log(myarr.includes(10));
+console.log(myarr.indexOf(11));
+console.log(myarr);
