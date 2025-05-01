eb75d6e5bbe35dbca179885cb64c9e8d3fbf8e35
Faisal Raza <faisalraza841311@gmail.com>
2025-05-01 15:21:21 +0000

    adding some array methods

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,13 +1,24 @@
-const myarr = [3,3,4,5,2,6];
+const myarr = [1,2,3,4,5,6];
 
-myarr.push(10);  // adding element to the last of the array
-myarr.push(23);  
-myarr.pop();  // removing array from last of the array
-myarr.unshift(90); // adding element  in starting of the array
-myarr.shift();  // removing  element  in starting of the array
-myarr.shift();
-myarr.shift()
+// myarr.push(10);  // adding element to the last of the array
+// myarr.push(23);  
+// myarr.pop();  // removing array from last of the array
+// myarr.unshift(90); // adding element  in starting of the array
+// myarr.shift();  // removing  element  in starting of the array
+// myarr.shift();
+// myarr.shift()
 
-console.log(myarr.includes(10));
-console.log(myarr.indexOf(11));
-console.log(myarr);
+// console.log(myarr.includes(11));  // finding elements in array 
+// console.log(myarr.indexOf(11));
+// console.log(myarr);
+
+console.log("A ", myarr);
+
+const myna1 = myarr.slice(1,3);
+console.log(myna1);
+
+console.log("B ", myarr);
+
+const myna2 = myarr.splice(1,3);
+console.log("C ", myarr);
+console.log(myna2);
