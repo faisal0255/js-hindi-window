2885372eeae6365fcdf4e983fae205f1c1c3b223
Faisal Raza <faisalraza841311@gmail.com>
2025-05-03 12:34:32 +0530

    adding info related to objects literals

---
+++ 02_basics/03_object.js
@@ -0,0 +1,11 @@
+// object literals..
+
+const newUser = {
+   name : "Faisal Raza",
+   age : 18,
+   college : "RGPV",
+   email : "faisal@google.com",
+   city : "Patna, Bihar",
+   isLoggedIn : "false",
+   lastLoggedIn : "3 days ago",
+};
