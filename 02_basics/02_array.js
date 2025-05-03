87921008bd379ee3be2b2e1f1a35d99341d69d1d
Faisal Raza <faisalraza841311@gmail.com>
2025-05-03 12:21:57 +0530

    adding some array checking

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -10,3 +10,5 @@ const newArr2 = [3,4,2,5,2];
 
 const outputArr = newArr1.concat(newArr2);  // adding two array using concat method without adding with the brackets
 console.log(outputArr);
+
+console.log(Array.isArray([3,4,5,2,5,32]));
