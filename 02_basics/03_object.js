157e8ba93b93d6e4d3263a4fd4751137ee6ff586
Faisal Raza <faisalraza841311@gmail.com>
2025-05-06 14:49:53 +0000

    something more related to object

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -9,3 +9,15 @@ const newUser = {
    isLoggedIn : "false",
    lastLoggedIn : "3 days ago",
 };
+
+console.log(newUser.email);
+
+newUser.email= "faisal@chatgpt.com"
+console.log(newUser.email);
+
+object.freeze(newUser)  // freezeing is used for not change in objects further
+
+newUser.name = "Rashid Kamal";
+
+console.log(newUser);
+console.log(newUser.name);
