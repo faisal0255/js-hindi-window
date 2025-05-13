c7f33ba23966a2285ef433a430d5f86cffed5e61
Faisal Raza <faisalraza841311@gmail.com>
2025-05-13 18:20:06 +0000

    switches in place of if else condition

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {   // structure for checking multiple condition..
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 5;
+
+switch (month) {
+    case 1 :
+        console.log("Jan");
+        break;
+    case 2 :
+        console.log("Feb");
+        break;
+    case 3 :
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default Executed");
+        
+        break;
+}
