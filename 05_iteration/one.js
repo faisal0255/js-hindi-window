1769ec7bc1cec04e4c050e47f84ca8a3c75f2bd4
Faisal Raza <faisalraza841311@gmail.com>
2025-05-18 07:10:05 +0000

    inproving onejs

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -30,3 +30,28 @@ for (let index = 1; index <= 15; index++) {
     // console.log(index);
     
 }
+
+// for of loop
+
+const arr = ["Tamil" , "Hindi", "Bhojpuri", "Bengali"]
+ 
+//const arr = [1,3,4,5,6]
+
+for (const element of arr) {
+    //console.log(element);
+}
+
+const greeting = "Hello World"
+
+for (const greet of greeting) {
+//  console.log(`This is greeting from me ${greet}`)
+// }
+
+const myName = "Faisal Raza"
+
+for (const letters of myName) {
+   if (letters === " ") {         // skipping the space between two name....
+    continue;
+   }
+    console.log(letters);  
+}
