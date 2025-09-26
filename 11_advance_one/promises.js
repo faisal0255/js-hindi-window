17f6f0a5d0604f5d78e1c54d1e4cc81ed77236a4
Faisal Raza <faisalraza841311@gmail.com>
2025-09-26 19:29:41 +0000

    promises in js

---
+++ 11_advance_one/promises.js
@@ -0,0 +1,29 @@
+const promiseOne = new Promise (function(resolve,reject) {
+    setTimeout(function() {
+        console.log("this is the first promise")
+    }, 1000);
+    resolve();
+});
+
+promiseOne.then(function(){
+    console.log("promise one accepted");
+});
+
+const promiseTwo = new Promise (function(resolve, reject) {
+    setTimeout(function(){
+        let error = false;
+        if(!error) {
+            resolve({username:"faisal raza", pass:"faisal123"})
+        } else {
+            reject('ERROR: Something is wrong')
+        }
+    },1000)
+})
+
+promiseTwo.then(function(user) {
+    console.log(user);
+    return user.username
+})
+.then((username) => console.log(username))
+.catch((err)=> console.log(err))
+.finally(()=> console.log("Answer is here"))
