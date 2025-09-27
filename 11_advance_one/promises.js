d5fe054d4cdad6d2ca0d5e2ebedd8da24ce3c40b
Faisal Raza <faisalraza841311@gmail.com>
2025-09-27 05:23:10 +0000

    async await

--- 11_advance_one/promises.js
+++ 11_advance_one/promises.js
@@ -1,29 +1,51 @@
-const promiseOne = new Promise (function(resolve,reject) {
-    setTimeout(function() {
-        console.log("this is the first promise")
-    }, 1000);
-    resolve();
-});
+// const promiseOne = new Promise (function(resolve,reject) {
+//     setTimeout(function() {
+//         console.log("this is the first promise")
+//     }, 1000);
+//     resolve();
+// });
 
-promiseOne.then(function(){
-    console.log("promise one accepted");
-});
+// promiseOne.then(function(){
+//     console.log("promise one accepted");
+// });
 
-const promiseTwo = new Promise (function(resolve, reject) {
+// const promiseTwo = new Promise (function(resolve, reject) {
+//     setTimeout(function(){
+//         let error = false;
+//         if(!error) {
+//             resolve({username:"faisal raza", pass:"faisal123"})
+//         } else {
+//             reject('ERROR: Something is wrong')
+//         }
+//     },1000)
+// })
+
+// promiseTwo.then(function(user) {
+//     console.log(user);
+//     return user.username
+// })
+// .then((username) => console.log(username))
+// .catch((err)=> console.log(err))
+// .finally(()=> console.log("Answer is here"))
+
+const promiseThree = new Promise (function(resolve,reject) {
     setTimeout(function(){
         let error = false;
         if(!error) {
-            resolve({username:"faisal raza", pass:"faisal123"})
+            resolve({username:"JS", pass:"121"})
         } else {
-            reject('ERROR: Something is wrong')
+            reject("ERROR: JS is wrong")
         }
-    },1000)
-})
+    },1000);
+});
 
-promiseTwo.then(function(user) {
-    console.log(user);
-    return user.username
-})
-.then((username) => console.log(username))
-.catch((err)=> console.log(err))
-.finally(()=> console.log("Answer is here"))
+async function consumePromiseThree() {
+    try {
+        const response = await promiseThree
+        console.log(response)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseThree();
